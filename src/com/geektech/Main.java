d538c4d09eaeac4f5b095b643d2c88d9f6248249
TroJIJI <adisrahimov0301@gmail.com>
2021-04-25 16:59:56 +0600

    Initial commit

---
+++ src/com/geektech/Main.java
@@ -0,0 +1,30 @@
+package com.geektech;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BankAccount bankAccount = new BankAccount();
+        bankAccount.deposit(20000);
+
+
+
+        try {
+            bankAccount.withDraw(6000);
+            bankAccount.withDraw((int) bankAccount.getAmount());
+        } catch (LimitException limitExeption) {
+            System.out.println(limitExeption.getMessage());
+            System.out.println("Доступный баланс для снятия: " + bankAccount.getAmount() + "\n Ваш баланс после снятия доступного баланса: " + (bankAccount.getAmount() - bankAccount.getAmount()));
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
+
+
